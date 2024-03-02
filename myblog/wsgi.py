6f4d3e1f79e9cf2d50d5268e56da08122c2f0131
Vinith Kumar Pasula <vinithkumarpasula@Viniths-MacBook-Air.local>
2024-03-02 13:08:51 -0500

    adding additional files for deployment

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+settings_module = 'myblog.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'myblog.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
